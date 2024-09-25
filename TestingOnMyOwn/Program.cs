6f4af08494f9a217308de17768a08a49260b9138
MarenSusanne <Marensut@getacademy.no>
2024-09-25 12:38:13 +0200

    Add project files.

---
+++ TestingOnMyOwn/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace TestingOnMyOwn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string connectionString = "Server=MST\\SQLEXPRESS;Database=DogTraining;Trusted_Connection=True;TrustServerCertificate=True;";
+
+            string query = "SELECT DISTINCT club FROM User_Info";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    Console.WriteLine("Connection successful!");
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Console.WriteLine($"Club: {reader["Club"]}");
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+    }
+}
